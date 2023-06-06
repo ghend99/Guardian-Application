946b55d95ddcc23a4541ec5fa09329344413c8ec
Ethan Pratt <ethan.pratt@ymail.com>
2023-06-06 08:53:38 +0100

    Queried login modal fullname input

--- .history/elements_20230606085317.js
+++ .history/elements_20230606085317.js
@@ -76,4 +76,4 @@ const resetPinNewRetype = document.getElementById("password-reset-new-retype");
 
 const btnResetPinSubmit = document.getElementById("password-reset-submit");
 
-const loginModalFullNameInput = document.getElementById("login-modal-fullname");
+const loginModalFullNameInput = document.getElementById("");
