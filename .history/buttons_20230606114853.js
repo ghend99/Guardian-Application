fd72e6f86fb6df8093f1a0e3b4e105dd9229fc85
Ethan Pratt <ethan.pratt@ymail.com>
2023-06-06 12:27:17 +0100

    Updated history

---
+++ .history/buttons_20230606114853.js
@@ -0,0 +1,85 @@
+btnDarkModeToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (darkMode === false) {
+    darkModeToggle();
+  } else {
+    lightModeToggle();
+  }
+});
+
+btnAddStudentOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  addStudentContainer.style.display = "block";
+  mainContainer.style.filter = "blur(3px)";
+});
+
+btnAddStudentClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  addStudentContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btndAddStudentSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  createStudent();
+  addStudentFirstNameInput.value = "";
+  addStudentLastNameInput.value = "";
+  addStudentYearInput.value = "";
+  addStudentPinInput.value = "";
+  addStudentRePinInput.value = "";
+});
+
+btnLoginHeader.addEventListener("click", function (e) {
+  // e.preventDefault();
+  login();
+  studentInformation.forEach((e) => {
+    e.style.display = "block";
+    inboxContainer.style.display = "none";
+  });
+});
+
+btnLoginModalOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginContainer.style.display = "block";
+  mainContainer.style.filter = "blur(3px)";
+});
+
+btnLoginModalClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btnLoginModalSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginModal();
+  studentInformation.forEach((e) => {
+    e.style.display = "block";
+  });
+  loginContainer.style.display = "none";
+});
+
+btnComposeMessageOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (currentAccount === undefined) {
+    alert("You need to login first");
+  } else {
+    composeMessageContainer.style.display = "block";
+    mainContainer.style.filter = "blur(3px)";
+  }
+});
+
+btnComposeMessageClose.addEventListener("click", function (e) {
+  composeMessageContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btnComposeMessageSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  sendMessage();
+});
+
+btnInboxOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  inboxContainer.style.display = "block";
+});
---
+++ .history/message_20230606113551.js
@@ -0,0 +1,53 @@
+const sendMessage = function () {
+  const message = composeMessageText.value;
+  const subject = composeMessageSubject.value;
+  const reciever = composeMessageReciever.value;
+  const target = students.find((stu) => stu.fullName === `${reciever}`);
+  const date = new Date(Date.now()).toLocaleString();
+  console.log(date);
+  console.log(message, subject, reciever, target);
+  if (target === undefined) {
+    alert("Not a valid message reciercer");
+  } else {
+    target.inbox.push(message);
+    target.from.push(currentAccount.fullName);
+    target.subject.push(subject);
+    target.date.push(date);
+    // currentAccount.sent.push(message);
+    // currentAccount.date.push(date);
+    composeMessageReciever.value = "";
+    composeMessageText.value = "";
+    composeMessageSubject.value = "";
+    localStorage.setItem("students", JSON.stringify(students));
+    alert("Message Sent");
+    // updateInbox(target, currentAccount, message, subject);
+  }
+};
+
+// const updateInbox = function (target, currentAccount, message, subject) {
+//   const html = ` <div id="inbox-line">
+//   <p>${new Date(Date.now()).toLocaleString()}</p>
+//   <p>${currentAccount.fullName}</p>
+//   <p>${target.fullName}</p>
+//   <p>${subject}</p>
+//   <p>${message}</p>
+// </div>
+//   `;
+//   inboxLine.insertAdjacentHTML("beforebegin", html);
+// };
+
+const updateInboxLoad = function (target) {
+  getStudentsLocalStorage();
+  for (let i = 0; i < target.inbox.length; i++) {
+    const html = `<div id="inbox-line">
+  <p>${currentAccount.date}</p>
+  <p>${currentAccount.from}</p>
+  <p>${currentAccount.fullName}</p>
+  <p>${currentAccount.subject}</p>
+  <p>${currentAccount.inbox[i]}</p>
+</div> 
+  `;
+    inboxLine.insertAdjacentHTML("beforebegin", html[i]);
+  }
+};
+// updateInboxLoad();
