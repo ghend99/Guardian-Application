2c1ea44df6367643c5351534857423d4b1ccba6b
Ethan Pratt <ethan.pratt@ymail.com>
2023-06-06 10:07:02 +0100

    Updating history file

---
+++ .history/buttons_20230606094407.js
@@ -0,0 +1,67 @@
+btnDarkModeToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (darkMode === false) {
+    darkModeToggle();
+  } else {
+    lightModeToggle();
+  }
+});
+
+btnAddStudentOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  addStudentContainer.style.display = "block";
+  mainContainer.style.filter = "blur(3px)";
+});
+
+btnAddStudentClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  addStudentContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btnLoginHeader.addEventListener("click", function (e) {
+  e.preventDefault();
+  login();
+  studentInformation.forEach((e) => {
+    e.style.display = "block";
+  });
+});
+
+btnLoginModalOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginContainer.style.display = "block";
+  mainContainer.style.filter = "blur(3px)";
+});
+
+btnLoginModalClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btnLoginModalSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginModal();
+  studentInformation.forEach((e) => {
+    e.style.display = "block";
+  });
+});
+
+btnComposeMessageOpen.addEventListener("click", function (e) {
+  e.preventDefault();
+  composeMessageContainer.style.display = "block";
+  mainContainer.style.filter = "blur(3px)";
+  if (currentAccount === undefined) {
+    alert("You need to login first");
+  }
+});
+
+btnComposeMessageClose.addEventListener("click", function (e) {
+  composeMessageContainer.style.display = "none";
+  mainContainer.style.filter = "blur(0px)";
+});
+
+btnComposeMessageSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  sendMessage();
+});
